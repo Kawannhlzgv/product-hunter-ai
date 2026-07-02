286bd41be527bc2674dec3d205f2a70857691b8a
Kawannhlzgv <Kawannholzlechner@gmail.com>
2026-07-01 21:59:57 -0300

    Delete src/app directory

--- src/app/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="pt-br">
-      <body style={{ margin: 0 }}>
-        {children}
-      </body>
-    </html>
-  );
-}
--- src/app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <h1>ProductHunter AI rodando 🚀</h1>;
-}
